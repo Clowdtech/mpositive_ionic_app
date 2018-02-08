2870939bb2197794bcb0ac25c61cfae20e1f8f9f
ovykhopen <Oleh.Vykhopen@symphony-solutions.eu>
2018-02-08 21:25:50 +0200

    fixed menu component after ionic upgrading to version 3.7.0

--- ionic-components/menu-v3/menu-gestures.js
+++ ionic-components/menu-v3/menu-gestures.js
@@ -1,100 +1,153 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
+      var extendStatics = Object.setPrototypeOf ||
+          ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+          function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+      return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-import { GESTURE_MENU_SWIPE, GESTURE_PRIORITY_MENU_SWIPE } from '../../gestures/gesture-controller';
+      };
+    })();
+import { GESTURE_PRIORITY_MENU_SWIPE, GESTURE_MENU_SWIPE } from '../../gestures/gesture-controller';
 import { SlideEdgeGesture } from '../../gestures/slide-edge-gesture';
 /**
  * Gesture attached to the content which the menu is assigned to
  */
 var MenuContentGesture = (function (_super) {
-    __extends(MenuContentGesture, _super);
-    function MenuContentGesture(plt, menu, gestureCtrl, domCtrl) {
-        var _this = _super.call(this, plt, plt.doc().body, {
-            direction: 'x',
-            edge: menu.side,
-            threshold: 5,
-            maxEdgeStart: menu.maxEdgeStart || 50,
-            zone: false,
-            passive: true,
-            domController: domCtrl,
-            gesture: gestureCtrl.createGesture({
-                name: GESTURE_MENU_SWIPE,
-                priority: GESTURE_PRIORITY_MENU_SWIPE,
-                disableScroll: true
-            })
+  __extends(MenuContentGesture, _super);
+  /**
+   * @param {?} plt
+   * @param {?} menu
+   * @param {?} gestureCtrl
+   * @param {?} domCtrl
+   */
+  function MenuContentGesture(plt, menu, gestureCtrl, domCtrl) {
+    var _this = _super.call(this, plt, plt.doc().body, {
+          direction: menu.direction === 'vertical' ? 'y' : 'x',
+          edge: menu.side,
+          threshold: 5,
+          maxEdgeStart: menu.maxEdgeStart || 50,
+          zone: false,
+          passive: true,
+          domController: domCtrl,
+          gesture: gestureCtrl.createGesture({
+            name: GESTURE_MENU_SWIPE,
+            priority: GESTURE_PRIORITY_MENU_SWIPE,
+            disableScroll: true
+          })
         }) || this;
-        _this.menu = menu;
-        return _this;
+    _this.menu = menu;
+    return _this;
+  }
+  /**
+   * @param {?} ev
+   * @return {?}
+   */
+  MenuContentGesture.prototype.canStart = function (ev) {
+    var /** @type {?} */ menu = this.menu;
+    if (!menu.canSwipe()) {
+      return false;
     }
-    MenuContentGesture.prototype.canStart = function (ev) {
-        var menu = this.menu;
-        if (!menu.canSwipe()) {
-            return false;
-        }
-        if (menu.isOpen) {
-            return true;
-        }
-        else if (menu.getMenuController().getOpen()) {
-            return false;
-        }
-        return _super.prototype.canStart.call(this, ev);
-    };
-    // Set CSS, then wait one frame for it to apply before sliding starts
-    MenuContentGesture.prototype.onSlideBeforeStart = function () {
-        (void 0) /* console.debug */;
-        this.menu._swipeBeforeStart();
-    };
-    MenuContentGesture.prototype.onSlideStart = function () {
-        (void 0) /* console.debug */;
-        this.menu._swipeStart();
-    };
-    MenuContentGesture.prototype.onSlide = function (slide) {
-        var z = (this.menu.isRightSide !== this.plt.isRTL ? slide.min : slide.max);
-        var stepValue = (slide.distance / z);
-        this.menu._swipeProgress(stepValue);
-    };
-    MenuContentGesture.prototype.onSlideEnd = function (slide) {
-        var z = (this.menu.isRightSide !== this.plt.isRTL ? slide.min : slide.max);
-        var currentStepValue = (slide.distance / z);
-        var velocity = slide.velocity;
-        z = Math.abs(z * 0.5);
-        var shouldCompleteRight = (velocity >= 0)
-            && (velocity > 0.2 || slide.delta > z);
-        var shouldCompleteLeft = (velocity <= 0)
-            && (velocity < -0.2 || slide.delta < -z);
-        (void 0) /* console.debug */;
-        this.menu._swipeEnd(shouldCompleteLeft, shouldCompleteRight, currentStepValue, velocity);
-    };
-    MenuContentGesture.prototype.getElementStartPos = function (slide) {
-        var menu = this.menu;
-        if (menu.isRightSide !== this.plt.isRTL) {
-            return menu.isOpen ? slide.min : slide.max;
-        }
-        // left menu
-        return menu.isOpen ? slide.max : slide.min;
-    };
-    MenuContentGesture.prototype.getSlideBoundaries = function () {
-        var menu = this.menu;
-        if (menu.isRightSide !== this.plt.isRTL) {
-            return {
-                min: -menu.width(),
-                max: 0
-            };
-        }
-        // left menu
+    if (menu.isOpen) {
+      return true;
+    }
+    else if (menu.getMenuController().getOpen()) {
+      return false;
+    }
+    return _super.prototype.canStart.call(this, ev);
+  };
+  /**
+   * @param {?} ev
+   * @return {?}
+   */
+  MenuContentGesture.prototype.onSlideBeforeStart = function (ev) {
+    (void 0) /* console.debug */;
+    this.menu._swipeBeforeStart();
+  };
+  /**
+   * @return {?}
+   */
+  MenuContentGesture.prototype.onSlideStart = function () {
+    (void 0) /* console.debug */;
+    this.menu._swipeStart();
+  };
+  /**
+   * @param {?} slide
+   * @param {?} ev
+   * @return {?}
+   */
+  MenuContentGesture.prototype.onSlide = function (slide, ev) {
+    var /** @type {?} */ z = (this.menu.side === 'right' || this.menu.side === 'bottom' ? slide.min : slide.max);
+    var /** @type {?} */ stepValue = (slide.distance / z);
+    this.menu._swipeProgress(stepValue);
+  };
+  /**
+   * @param {?} slide
+   * @param {?} ev
+   * @return {?}
+   */
+  MenuContentGesture.prototype.onSlideEnd = function (slide, ev) {
+    var z = (this.menu.side === 'right' || this.menu.side === 'bottom' ? slide.min : slide.max);
+    var currentStepValue = (slide.distance / z);
+    var velocity = slide.velocity;
+    z = Math.abs(z * 0.5);
+    var shouldCompleteBottom;
+    var shouldCompleteRight = shouldCompleteBottom = (velocity >= 0)
+        && (velocity > 0.2 || slide.delta > z);
+    var shouldCompleteTop;
+    var shouldCompleteLeft = shouldCompleteTop = (velocity <= 0)
+        && (velocity < -0.2 || slide.delta < -z);
+    (void 0) /* console.debug */;
+    this.menu._swipeEnd(shouldCompleteLeft, shouldCompleteRight, shouldCompleteTop, shouldCompleteBottom,
+        currentStepValue, velocity);
+  };
+  /**
+   * @param {?} slide
+   * @param {?} ev
+   * @return {?}
+   */
+  MenuContentGesture.prototype.getElementStartPos = function (slide, ev) {
+    if (this.menu.side === 'right' || this.menu.side === 'bottom') {
+      return this.menu.isOpen ? slide.min : slide.max;
+    }
+    // left && top menu
+    return this.menu.isOpen ? slide.max : slide.min;
+  };
+  /**
+   * @return {?}
+   */
+  MenuContentGesture.prototype.getSlideBoundaries = function () {
+    if (this.menu.direction === 'vertical') {
+      if (this.menu.side === 'bottom') {
         return {
-            min: 0,
-            max: menu.width()
+          min: -this.menu.height(),
+          max: 0
         };
-    };
-    return MenuContentGesture;
+      }
+      // top menu
+      return {
+        min: 0,
+        max: this.menu.height()
+      };
+    } else {
+      if (this.menu.side === 'right') {
+        return {
+          min: -this.menu.width(),
+          max: 0
+        };
+      }
+      // left menu
+      return {
+        min: 0,
+        max: this.menu.width()
+      };
+    }
+  };
+  return MenuContentGesture;
 }(SlideEdgeGesture));
 export { MenuContentGesture };
+function MenuContentGesture_tsickle_Closure_declarations() {
+  /** @type {?} */
+  MenuContentGesture.prototype.menu;
+}
 //# sourceMappingURL=menu-gestures.js.map
--- ionic-components/menu-v3/menu-types.js
+++ ionic-components/menu-v3/menu-types.js
@@ -1,13 +1,13 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
+      var extendStatics = Object.setPrototypeOf ||
+          ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+          function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+      return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+      };
+    })();
 import { Animation } from '../../animations/animation';
 import { MenuController } from '../app/menu-controller';
 /**
@@ -18,57 +18,90 @@ import { MenuController } from '../app/menu-controller';
  * and registers itself with Menu.
  */
 var MenuType = (function () {
-    function MenuType(plt) {
-        this.ani = new Animation(plt);
-        this.ani
-            .easing('cubic-bezier(0.0, 0.0, 0.2, 1)')
-            .easingReverse('cubic-bezier(0.4, 0.0, 0.6, 1)')
-            .duration(280);
+  /**
+   * @param {?} plt
+   */
+  function MenuType(plt) {
+    this.ani = new Animation(plt);
+    this.ani
+        .easing('cubic-bezier(0.0, 0.0, 0.2, 1)')
+        .easingReverse('cubic-bezier(0.4, 0.0, 0.6, 1)')
+        .duration(280);
+  }
+  /**
+   * @param {?} shouldOpen
+   * @param {?} animated
+   * @param {?} done
+   * @return {?}
+   */
+  MenuType.prototype.setOpen = function (shouldOpen, animated, done) {
+    var /** @type {?} */ ani = this.ani
+        .onFinish(done, true, true)
+        .reverse(!shouldOpen);
+    if (animated) {
+      ani.play();
     }
-    MenuType.prototype.setOpen = function (shouldOpen, animated, done) {
-        var ani = this.ani
-            .onFinish(done, true, true)
-            .reverse(!shouldOpen);
-        if (animated) {
-            ani.play();
-        }
-        else {
-            ani.syncPlay();
-        }
-    };
-    MenuType.prototype.setProgressStart = function (isOpen) {
-        this.isOpening = !isOpen;
-        // the cloned animation should not use an easing curve during seek
-        this.ani
-            .reverse(isOpen)
-            .progressStart();
-    };
-    MenuType.prototype.setProgessStep = function (stepValue) {
-        // adjust progress value depending if it opening or closing
-        this.ani.progressStep(stepValue);
-    };
-    MenuType.prototype.setProgressEnd = function (shouldComplete, currentStepValue, velocity, done) {
-        var _this = this;
-        var isOpen = (this.isOpening && shouldComplete);
-        if (!this.isOpening && !shouldComplete) {
-            isOpen = true;
-        }
-        var ani = this.ani;
-        ani.onFinish(function () {
-            _this.isOpening = false;
-            done(isOpen);
-        }, true);
-        var factor = 1 - Math.min(Math.abs(velocity) / 4, 0.7);
-        var dur = ani.getDuration() * factor;
-        ani.progressEnd(shouldComplete, currentStepValue, dur);
-    };
-    MenuType.prototype.destroy = function () {
-        this.ani.destroy();
-        this.ani = null;
-    };
-    return MenuType;
+    else {
+      ani.syncPlay();
+    }
+  };
+  /**
+   * @param {?} isOpen
+   * @return {?}
+   */
+  MenuType.prototype.setProgressStart = function (isOpen) {
+    this.isOpening = !isOpen;
+    // the cloned animation should not use an easing curve during seek
+    this.ani
+        .reverse(isOpen)
+        .progressStart();
+  };
+  /**
+   * @param {?} stepValue
+   * @return {?}
+   */
+  MenuType.prototype.setProgessStep = function (stepValue) {
+    // adjust progress value depending if it opening or closing
+    this.ani.progressStep(stepValue);
+  };
+  /**
+   * @param {?} shouldComplete
+   * @param {?} currentStepValue
+   * @param {?} velocity
+   * @param {?} done
+   * @return {?}
+   */
+  MenuType.prototype.setProgressEnd = function (shouldComplete, currentStepValue, velocity, done) {
+    var _this = this;
+    var /** @type {?} */ isOpen = (this.isOpening && shouldComplete);
+    if (!this.isOpening && !shouldComplete) {
+      isOpen = true;
+    }
+    var /** @type {?} */ ani = this.ani;
+    ani.onFinish(function () {
+      _this.isOpening = false;
+      done(isOpen);
+    }, true);
+    var /** @type {?} */ factor = 1 - Math.min(Math.abs(velocity) / 4, 0.7);
+    var /** @type {?} */ dur = ani.getDuration() * factor;
+    ani.progressEnd(shouldComplete, currentStepValue, dur);
+  };
+  /**
+   * @return {?}
+   */
+  MenuType.prototype.destroy = function () {
+    this.ani && this.ani.destroy();
+    this.ani = null;
+  };
+  return MenuType;
 }());
 export { MenuType };
+function MenuType_tsickle_Closure_declarations() {
+  /** @type {?} */
+  MenuType.prototype.ani;
+  /** @type {?} */
+  MenuType.prototype.isOpening;
+}
 /**
  * @hidden
  * Menu Reveal Type
@@ -76,16 +109,27 @@ export { MenuType };
  * The menu itself, which is under the content, does not move.
  */
 var MenuRevealType = (function (_super) {
-    __extends(MenuRevealType, _super);
-    function MenuRevealType(menu, plt) {
-        var _this = _super.call(this, plt) || this;
-        var openedX = (menu.width() * (menu.isRightSide ? -1 : 1)) + 'px';
-        var contentOpen = new Animation(plt, menu.getContentElement());
-        contentOpen.fromTo('translateX', '0px', openedX);
-        _this.ani.add(contentOpen);
-        return _this;
+  __extends(MenuRevealType, _super);
+  /**
+   * @param {?} menu
+   * @param {?} plt
+   */
+  function MenuRevealType(menu, plt) {
+    var _this = _super.call(this, plt) || this;
+    var opened;
+    var contentOpen = new Animation(plt, menu.getContentElement());
+    if (menu.horizontal === 'vertical') {
+      opened = (menu.height() * (menu.side === 'top' ? 1 : -1)) + 'px';
+      direction = 'translateY';
+    } else {
+      opened = (menu.width() * (menu.side === 'right' ? -1 : 1)) + 'px';
+      direction = 'translateX';
     }
-    return MenuRevealType;
+    contentOpen.fromTo(direction, '0px', opened);
+    _this.ani.add(contentOpen);
+    return _this;
+  }
+  return MenuRevealType;
 }(MenuType));
 MenuController.registerType('reveal', MenuRevealType);
 /**
@@ -95,31 +139,55 @@ MenuController.registerType('reveal', MenuRevealType);
  * The menu itself also slides over to reveal its bad self.
  */
 var MenuPushType = (function (_super) {
-    __extends(MenuPushType, _super);
-    function MenuPushType(menu, plt) {
-        var _this = _super.call(this, plt) || this;
-        var contentOpenedX, menuClosedX, menuOpenedX;
-        var width = menu.width();
-        if (menu.isRightSide) {
-            // right side
-            contentOpenedX = -width + 'px';
-            menuClosedX = width + 'px';
-            menuOpenedX = '0px';
-        }
-        else {
-            contentOpenedX = width + 'px';
-            menuOpenedX = '0px';
-            menuClosedX = -width + 'px';
-        }
-        var menuAni = new Animation(plt, menu.getMenuElement());
-        menuAni.fromTo('translateX', menuClosedX, menuOpenedX);
-        _this.ani.add(menuAni);
-        var contentApi = new Animation(plt, menu.getContentElement());
-        contentApi.fromTo('translateX', '0px', contentOpenedX);
-        _this.ani.add(contentApi);
-        return _this;
+  __extends(MenuPushType, _super);
+  /**
+   * @param {?} menu
+   * @param {?} plt
+   */
+  function MenuPushType(menu, plt) {
+    let _this = _super.call(this, plt);
+    var contentOpened, menuClosed, menuOpened, direction;
+    if (menu.direction === 'vertical') {
+      var contentOpenedY, menuClosedY, menuOpenedY;
+      if (menu.side === 'top') {
+        contentOpenedY = menu.height() + 'px';
+        menuOpenedY = '0px';
+        menuClosedY = -menu.height() + 'px';
+      } else {
+        contentOpenedY = -menu.height() + 'px';
+        menuOpenedY = '0px';
+        menuClosedY = menu.height() + 'px';
+      }
+      contentOpened = contentOpenedY;
+      menuClosed = menuClosedY;
+      menuOpened = menuOpenedY;
+      direction = 'translateY';
+    } else {
+      var contentOpenedX, menuClosedX, menuOpenedX;
+      if (menu.side === 'right') {
+        contentOpenedX = -menu.width() + 'px';
+        menuClosedX = menu.width() + 'px';
+        menuOpenedX = '0px';
+      }
+      else {
+        contentOpenedX = menu.width() + 'px';
+        menuOpenedX = '0px';
+        menuClosedX = -menu.width() + 'px';
+      }
+      contentOpened = contentOpenedX;
+      menuClosed = menuClosedX;
+      menuOpened = menuOpenedX;
+      direction = 'translateX';
     }
-    return MenuPushType;
+    var menuAni = new Animation(plt, menu.getMenuElement());
+    menuAni.fromTo(direction, menuClosed, menuOpened);
+    this.ani.add(menuAni);
+    var contentApi = new Animation(plt, menu.getContentElement());
+    contentApi.fromTo(direction, '0px', contentOpened);
+    this.ani.add(contentApi);
+    return _this;
+  }
+  return MenuPushType;
 }(MenuType));
 MenuController.registerType('push', MenuPushType);
 /**
@@ -129,30 +197,49 @@ MenuController.registerType('push', MenuPushType);
  * itself, which is under the menu, does not move.
  */
 var MenuOverlayType = (function (_super) {
-    __extends(MenuOverlayType, _super);
-    function MenuOverlayType(menu, plt) {
-        var _this = _super.call(this, plt) || this;
-        var closedX, openedX;
-        var width = menu.width();
-        if (menu.isRightSide) {
-            // right side
-            closedX = 8 + width + 'px';
-            openedX = '0px';
-        }
-        else {
-            // left side
-            closedX = -(8 + width) + 'px';
-            openedX = '0px';
-        }
-        var menuAni = new Animation(plt, menu.getMenuElement());
-        menuAni.fromTo('translateX', closedX, openedX);
-        _this.ani.add(menuAni);
-        var backdropApi = new Animation(plt, menu.getBackdropElement());
-        backdropApi.fromTo('opacity', 0.01, 0.35);
-        _this.ani.add(backdropApi);
-        return _this;
+  __extends(MenuOverlayType, _super);
+  /**
+   * @param {?} menu
+   * @param {?} plt
+   */
+  function MenuOverlayType(menu, plt) {
+    let _this = _super.call(this, plt);
+    var closed, opened, direction;
+    if (menu.direction === 'vertical') {
+      var closedY, openedY;
+      if (menu.side === 'top') {
+        closedY = -(8 + menu.height()) + 'px';
+        openedY = '0px';
+      } else {
+        closedY = 8 + menu.height() + 'px';
+        openedY = '0px';
+      }
+      closed = closedY;
+      opened = openedY;
+      direction = 'translateY';
+    } else {
+      var closedX, openedX;
+      if (menu.side === 'right') {
+        closedX = 8 + menu.width() + 'px';
+        openedX = '0px';
+      }
+      else {
+        closedX = -(8 + menu.width()) + 'px';
+        openedX = '0px';
+      }
+      closed = closedX;
+      opened = openedX;
+      direction = 'translateX';
     }
-    return MenuOverlayType;
+    var menuAni = new Animation(plt, menu.getMenuElement());
+    menuAni.fromTo(direction, closed, opened);
+    this.ani.add(menuAni);
+    var backdropApi = new Animation(plt, menu.getBackdropElement());
+    backdropApi.fromTo('opacity', 0.01, 0.35);
+    this.ani.add(backdropApi);
+    return _this;
+  }
+  return MenuOverlayType;
 }(MenuType));
 MenuController.registerType('overlay', MenuOverlayType);
 //# sourceMappingURL=menu-types.js.map
